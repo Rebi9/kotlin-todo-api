55d2df6e035adfca5bcf6beb8685a0bf3565688d
ryo_ebihara <ab.yr.2t@gmail.com>
2020-03-28 11:45:22 +0900

    feat(ToDoResponse):set format to LocalDateTime

--- src/main/kotlin/com/example/todo/presentation/reaponse/TodoResponse.kt
+++ src/main/kotlin/com/example/todo/presentation/reaponse/TodoResponse.kt
@@ -1,5 +1,6 @@
 package com.example.todo.presentation.reaponse
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDateTime
 
@@ -11,8 +12,10 @@ class TodoResponse(
         val isDone: Boolean,
 
         @JsonProperty("crated_At")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         val createdAt: LocalDateTime?,
 
         @JsonProperty("updated_At")
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         val updatedAt: LocalDateTime?
 )
