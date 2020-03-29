b4344eecc7287af76028f79a43c38f144b453051
ryo_ebihara <ab.yr.2t@gmail.com>
2020-03-29 10:56:50 +0900

    refactor:move controller class

--- src/main/kotlin/com/example/todo/presentation/controller/TestController.kt
+++ src/main/kotlin/com/example/todo/presentation/controller/TestController.kt
@@ -1,4 +1,4 @@
-package com.example.todo.presentation
+package com.example.todo.presentation.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
